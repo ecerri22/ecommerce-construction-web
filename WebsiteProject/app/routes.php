0a1c1c2df761eedf93698b594bd925dd298ab259
Enia Çerri <128168490+ecerri22@users.noreply.github.com>
2024-05-12 11:51:11 +0200

    all routes

--- WebsiteProject/app/routes.php
+++ WebsiteProject/app/routes.php
@@ -1,45 +1,70 @@
 <?php
-
-use Controllers\AddProductAdminController;
+use Controllers\AdminController;
 use Controllers\UserController;
-use Controllers\HomePageController;
-use Controllers\OrdersAdminController;
-use Controllers\ProductsAdminController;
-use Controllers\UsersAdminController;
-use Controllers\AdminDashboardController;
-use Controllers\ProductsUserController;
+use Controllers\MainPagesController;
+use Controllers\ProductController;
 
+//********USER********* //
 // HOME PAGE //
-$homepagecont = new HomePageController();
-$router->get('/', $homepagecont, 'renderHomePage');
+$mc = new MainPagesController();
+$router->get('/', $mc, 'renderHomePage');
 
+// ABOUT US PAGE //
+$router->get('/aboutUs', $mc, 'renderAboutUsPage');
+
+// ERROR PAGE //
+$router->get('/error', $mc, 'renderErrorPage');
 
 // SIGNUP PAGE //
 $uc = new UserController();
 $router->get('/signup', $uc, 'renderSignup');
 $router->post('/signup', $uc, 'signUp');
 
+// LOGIN PAGE //
+$router->get('/login', $uc, 'renderLogin');
 
+// HELP-ADVICE PAGE //
+$router->get('/helpAdvice', $uc, 'renderHelpAdvice');
+
+// MY ACCOUNT PAGE //
+$router->get('/myAccount',$uc,'renderMyAccount');
+
+// ORDER HISTORY PAGE //
+$router->get('/myOrders', $uc, 'renderMyOrders');
+
+// CHECKOUT PAGE //
+$router->get('/checkout', $uc, 'renderCheckout');
+
+// WISHLIST PAGE //
+$router->get('/wishlist', $uc, 'renderWishlist');
+
+// SHOPPING CART PAGE //
+$router->get('/shoppingCart', $uc, 'renderShoppingCart');
+
+// ALL PRODUCTS PAGE //
+$pc = new ProductController();
+$router->get('/allProducts', $pc, 'renderAllProductsPage');
+
+// PRODUCT DETAILS PAGE //
+$router->get('/productDetails', $pc, 'renderProductDetails');
+
+/********************************/
+//********ADMIN********* //
+$ac = new AdminController()
+;
 // ADMIN - CREATE PRODUCT //
-$adminAddProd = new AddProductAdminController();
-$router->get('/createProduct', $adminAddProd, 'renderAddProductPage');
-
-// ADMIN - ALL PRODUCTS //
-$prAdC = new ProductsAdminController();
-$router->get('/allProductsAdmin', $prAdC, 'renderProductsAdminController');
-
-// ADMIN - ALL ORDERS //
-$orAdC = new OrdersAdminController();
-$router->get('/allOrdersAdmin', $orAdC, 'renderOrdersAdminController');
-
-// ADMIN - ALL USERS //
-$usAdC = new UsersAdminController();
-$router->get('/allUsersAdmin', $usAdC, 'renderUsersAdminController');
+$router->get('/createProduct', $ac, 'renderAddProductPage');
 
 // ADMIN - DASHBOARD //
-$adminDashboard = new AdminDashboardController();
-$router->get('/adminDashboard', $adminDashboard, 'renderAdminDashboard');
+$router->get('/dashboard', $ac, 'renderDashboardAdminPage');
 
-// USER - PRODUCTS //
-$userProducts = new ProductsUserController();
-$router->get('/allProductsUser', $userProducts, 'renderProductsUserController');
+// ADMIN - ALL PRODUCTS //
+$router->get('/allProductsAdmin', $ac, 'renderProductsAdminController');
+
+// ADMIN - ALL ORDERS //
+$router->get('/allOrdersAdmin', $ac, 'renderOrdersAdminController');
+
+// ADMIN - ALL USERS //
+$router->get('/allUsersAdmin', $ac, 'renderUsersAdminController');
+
+
