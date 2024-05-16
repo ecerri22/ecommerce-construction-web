8e6874a79a19cb1c4f6757c40431119ba9e06eee
ThanasPapa <100229861+Slowbro213@users.noreply.github.com>
2024-05-16 17:31:16 +0200

    Products

--- WebsiteProject/app/Models/product.php
+++ WebsiteProject/app/Models/product.php
@@ -1,19 +1,21 @@
 <?php
 
 namespace Models;
+use Core\App;
 
 class Product{
-    private $name;
-    private $price;
-    private $image;
-    private $link;
-    private $category;
-    private $minidescription;
-    private static $products = array();
+    public $id;
+    public $name;
+    public $price;
+    public $image;
+    public $link;
+    public $category;
+    public $minidescription;
+    public static $products = array();
 
-
-    public function __construct($name, $price, $image, $link, $minidescription, $category)
+    public function __construct($id,$name, $price, $image, $link, $minidescription, $category)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->price = $price;
         $this->image = $image;
@@ -29,14 +31,21 @@ class Product{
         echo  '     <div class="product-description-container">';
         echo  '       <div class="description-box">';
         echo  '         <h3 style = "display : inline-block; margin-bottom: .4rem; ">';
-        echo  '            <a href="/#" class = "product-name">'.$this->name.'</a>';
+        echo  '            <a href="'.$this->link.'" class = "product-name">'.$this->name.'</a>';
         echo  '         </h3>';
         echo  '         <button class="compare-btn" onclick ="makesticky(this)">Hold</button>';
         echo  '         <p class="product-code">'.$this->minidescription.'</p>';
         echo  '         <p class="product-price">$'.$this->price.'</p>';
         echo  '       </div>';
         echo  '       <div class="wishlist-btn">';
-        echo  '         <button class="add-to-cart">Add to cart</button>';
+        if(isset($_SESSION['user']))
+        {
+            echo  '         <button class="add-to-cart" onclick="addtocart('.@$_SESSION['user']['user_id'].','.$this->id.');">Add to Cart</button>';
+        }
+        else
+        {
+            echo  '         <button class="add-to-cart" onclick="alert(\'Please login to add to wishlist\')">Add to Cart</button>';
+        }
         echo  '       </div>';
         echo  '     </div>';
         echo  ' </div>';
@@ -74,6 +83,21 @@ class Product{
     public function getMinidescription()
     {
         return $this->minidescription;
+    }
+
+    public static function getAllproducts()
+    {
+        $result = App::container()->resolve('Core\Database')->query('SELECT * FROM products
+        JOIN categories ON categories.category_id = products.category_id')->get(); 
+
+        // Iterate through the result and return an array of product objects
+        $products = array();
+        foreach ($result as $row) {
+            $product = new Product($row['product_id'],$row['name'], $row['price'], $row['product_image'],"/productDetails?productID=".$row['product_id'], $row['description'], $row['category_name']);
+            $products[] = $product;
+        }
+        return $products;
+         
     }
 
 }
