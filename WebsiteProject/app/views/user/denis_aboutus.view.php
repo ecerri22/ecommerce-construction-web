7c968670b6eddc0210d8b0e8e9da71b9a59f5359
Denis Lala <dlala22@epoka.edu.al>
2024-05-17 10:57:36 +0200

    Update denis_aboutus.view.php

--- WebsiteProject/app/views/user/denis_aboutus.view.php
+++ WebsiteProject/app/views/user/denis_aboutus.view.php
@@ -5,3 +5,24 @@ use Pages\AboutUsView;
 $aboutUsView = new AboutUsView("About Us", 'Denis/denisStyles.css');
 $aboutUsView->render();
 
+$query = "SELECT * FROM about_us_content";
+$query_run= mysqli_query($connection,$query);
+
+if(mysqli_num_rows($query_run))
+{
+
+
+foreach($query_run as $row)
+{
+   ?>
+   echo $row['id'];
+   echo  $row['section_name'];
+   echo $row['content'];
+   <?php
+}
+}
+else
+{
+    echo "No info found!!";
+}
+?>
