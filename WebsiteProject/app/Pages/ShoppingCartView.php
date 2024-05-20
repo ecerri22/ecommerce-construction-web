221883b38e72dea3ec61cfdecf40493841f8953e
ThanasPapa <100229861+Slowbro213@users.noreply.github.com>
2024-05-20 22:49:22 +0200

    empy cart fix

--- WebsiteProject/app/Pages/ShoppingCartView.php
+++ WebsiteProject/app/Pages/ShoppingCartView.php
@@ -24,6 +24,59 @@ class shoppingCartView{
       
       <h2 class="wishlist-title">Shopping Cart</h2>
       <div class="cart-container">
+      <?php if(count($allShoppingCartProducts) == 0) : ?>
+            <div class="nothing-to-show">
+                                <div class="container-nothing-to-show">
+                                    <h2 class="nothing-to-show-h2">Nothing to show here</h2>
+                                    <p class="no-prod-p-expl">You haven't added anything yet</p>
+                                    <a class="go-allprod-btn" href="/allProducts">Go back to All Products</a>
+                                </div>
+                            </div>
+                            <style>
+                                .nothing-to-show {
+                                    display: flex;
+                                    flex-direction: column;
+                                    align-items: center;
+                                    justify-content: center;
+                                    height: 50vh;
+                                }
+                                .container-nothing-to-show{
+                                    text-align: center;
+                                    color: #fd7e14;
+                                    display: flex;
+                                    flex-direction: column;
+                                    align-items: center;
+                                    justify-content: center;
+                                    gap: 2.1rem;
+                                }
+
+                                .go-allprod-btn {
+                                    text-decoration: none;
+                                    background: #061538; 
+                                    color: #fff; 
+                                    padding: 0.75rem 1.5rem; 
+                                    font-size: 0.9rem;
+                                    text-transform: uppercase;
+                                    transition: background-color 0.4s, color 0.4s;
+                                    cursor: pointer;
+                                    max-width: max-content;
+                                }
+
+                                .go-allprod-btn:hover {
+                                    background: #081f54;
+                                }
+
+                                .nothing-to-show-h2 {
+                                    font-size: 2.2rem;
+                                }
+
+                                .no-prod-p-expl {
+                                    font-size: 1rem;
+                                    color: #888;
+                                }
+
+                            </style>
+          <?php endif; ?>
       <div class="cart-products-container">
 
       <?php foreach($allShoppingCartProducts as $shoppingCartProduct) : ?>
@@ -62,7 +115,6 @@ class shoppingCartView{
         <?php endforeach ; ?>
 
             </div>
-
             <div class="price-container">
               <p class="order-summary">Order summary</p>
               <div class="order-info subtotal">
